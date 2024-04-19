42a65fd40dd265d13b518d6c64b1783e20b39906
nagathej <167506036+nagathej@users.noreply.github.com>
2024-04-19 13:26:13 +0530

    Update features path CustomRunner.java

--- src/test/java/Runner/CustomRunner.java
+++ src/test/java/Runner/CustomRunner.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @CucumberOptions (
-        features={"/Users/nkanikicharla/Desktop/TestNGCucumberProject/src/test/resources/Feature/Demo.feature"},
+        features={"/src/test/resources/Feature/Demo.feature"},
         tags="@smoke",
         plugin = {"pretty",
                 "html:target/cucumber/report.html",
